4a6e99cc29fa899cf4e949484965fe8c4cdfb890
chucklesoclock <charlieg895@gmail.com>
2017-09-22 17:29:40 -0500

    double braced the css sections to not interfere with .format

--- main.py
+++ main.py
@@ -3,34 +3,32 @@ from caesar import encrypt as caesar_encrypt
 
 app = Flask(__name__)
 
-style = """form {
+form = """<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
-            #rot_input {
+            }}
+            #rot_input {{
                 width: 3em;
                 border-radius: 0px;
-            }
-            #rot_input:invalid {
+            }}
+            #rot_input:invalid {{
                 border-right: 2px solid;
                 border-color: red;
-                
-            }"""
-form = """<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            {}
+            }}
         </style>
     </head>
     <body>
